6c13b35e197ccecc4dd79bf4d7486fa1fc5b4781
Clément 'birdy' Danjou <clement@danjou.io>
2017-04-14 11:25:52 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,41 @@
+import curry from 'curry';
+
+let moduleReducerKey = 'module';
+const moduleReducers = {};
+let reducerHasBeenGet = false;
+
+export const updateModuleReducerKey = (key) => {
+  moduleReducerKey = key;
+};
+
+export const getReducers = () => {
+  reducerHasBeenGet = true;
+  return moduleReducers
+};
+
+export const registerModuleReducer = (moduleName, reducer) => {
+  if (reducerHasBeenGet === true) {
+    console.warn(`It seems you try to register "${moduleName}" but reducers has been already get.`);
+  }
+  if (!moduleReducers[moduleName]) moduleReducers[moduleName] = reducer;
+  else console.warn(`The reducer "${moduleName}" is already registered. Please check you don't register your reducer twice.`);
+};
+
+export const getModuleState = curry((moduleName, state) => (
+  state[MODULE_REDUCER_KEY][moduleName]
+));
+
+const moduleSagas = [];
+let sagaHasBeenGet = false;
+
+export const getSagas = () => {
+  sagaHasBeenGet = true;
+  moduleSagas
+};
+
+export const registerModuleSaga = (saga) => {
+  if (reducerHasBeenGet === true) {
+    console.warn(`It seems you try to register "${moduleName}" but sagas has been already get.`);
+  }
+  moduleSagas.push(saga);
+};
